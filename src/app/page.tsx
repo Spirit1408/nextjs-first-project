5dabff79ac1c27ba91d91c26e4b30292d1a8b490
Oleh Shalashov <spirit14081992@gmail.com>
2025-02-09 14:23:35 -0300

    Update example of usage of client-side components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import { Status, StatusLabel } from "./components/status-label";
-import { cookies } from "next/headers";
+import AddCompanyButton from "./components/add-company-button";
 
 export default function Home() {
-	console.log(cookies());
-
 	return (
 		<main>
 			<h1 className="text-3xl font-bold underline mb-2">
 				Home page. Local time: {new Date().toTimeString()}
 			</h1>
-			<StatusLabel status={Status.Active}>Active</StatusLabel>
-			<StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-			<StatusLabel status={Status.Pending}>Pending</StatusLabel>
-			<StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+			<AddCompanyButton />
 		</main>
 	);
 }
