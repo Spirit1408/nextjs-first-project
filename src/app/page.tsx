8c74cc8bff8b6834888f641d739b3c5b2cbeb4f4
Oleh Shalashov <spirit14081992@gmail.com>
2025-02-09 11:24:26 -0300

    Add dynamic behavior to the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Status, StatusLabel } from "./components/status-label";
-
+import { cookies } from "next/headers";
 
 export default function Home() {
+	console.log(cookies());
+
 	return (
 		<main>
-			<h1 className="text-3xl font-bold underline mb-2">Home page</h1>
+			<h1 className="text-3xl font-bold underline mb-2">
+				Home page. Local time: {new Date().toTimeString()}
+			</h1>
 			<StatusLabel status={Status.Active}>Active</StatusLabel>
 			<StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
 			<StatusLabel status={Status.Pending}>Pending</StatusLabel>
