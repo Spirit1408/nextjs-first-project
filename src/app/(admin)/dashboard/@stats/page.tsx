f5a1b4f59eeeb87e602bde417db16b9623249736
Oleh Shalashov <spirit14081992@gmail.com>
2025-02-17 15:22:57 -0300

    Add API usage to segments, add fetching functions

--- src/app/(admin)/dashboard/@stats/page.tsx
+++ src/app/(admin)/dashboard/@stats/page.tsx
@@ -1,8 +1,8 @@
 import StatCard, { StatCardType } from "@/app/components/stat-card";
-import { getSummaryStats } from "@/lib/api";
+import { getSummaryStats, SummaryStats } from "@/lib/api";
 import Link from "next/link";
 
-const labelbyStat = {
+const labelbyStat: Record<keyof SummaryStats, string> = {
 	promotions: "Total promotions",
 	categories: "Total categories",
 	newCompanies: "New companies",
@@ -10,11 +10,13 @@ const labelbyStat = {
 };
 
 export default async function Page() {
-	const data = await getSummaryStats();
+	const data = await getSummaryStats({
+		next: { revalidate: 5 },
+	});
 
 	return (
 		<div className="grid grid-cols-12 gap-5">
-			{(Object.keys(data) as (keyof typeof data)[]).map((key) => (
+			{(Object.keys(data) as (keyof SummaryStats)[]).map((key) => (
 				<Link href={`/dashboard/${key}`} key={key} className="col-span-3">
 					<StatCard
 						type={StatCardType.Gradient}
